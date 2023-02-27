ddae77c6c432d1015dedcd3825690b0f499d5197
moscarce <oluwa14tobi@gmail.com>
2023-02-27 17:20:40 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Group2 from './assets/Group2.png'
+
+
+export default function App() {
+
+  return (
+    <div className="App">
+       <h1>hello</h1>
+       <img src={Group2} alt="" />
+    </div>
+  )
+}
